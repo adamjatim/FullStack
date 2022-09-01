eaaedffad7612ce576d7e28c165d2aeae742f01d
adam <adam@pop-os.localdomain>
2022-09-01 17:29:20 +0700

    Update

--- backend/config/Database.js
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from "sequelize";
-
-// creat db with name crud_db, username: root, password: NULL
-const db = new Sequelize('crud_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-export default db;
