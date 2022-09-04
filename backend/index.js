7f0fb4aa0af7401304bb853dde1acfd14b4695d5
adam <adam@pop-os.localdomain>
2022-09-05 05:57:54 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(UserRoute);
 
 app.listen(5000, () => console.log('server up and running...'))
