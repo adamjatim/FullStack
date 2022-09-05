672c916c70f554a8e19a9218cbfde54dd7d4edd3
adam <adam@pop-os.localdomain>
2022-09-06 00:53:44 +0700

    update routes

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getUsers } from "../controllers/UserController.js"
+import { 
+    getUsers,
+    getUsersById,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controllers/UserController.js"
 
 const router = express.Router();
 
 router.get('/Users', getUsers);
+router.get('/Users/:id', getUsersById);
+router.post('/Users', createUser);
+router.patch('/Users/:id', updateUser);
+router.delete('/Users/:id', deleteUser);
 
 export default router;
